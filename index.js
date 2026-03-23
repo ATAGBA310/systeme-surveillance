221c9f1c0aef286bfe866bc42f2280c2148ff850
JACARTISTE <mamessile.nimon@gmail.com>
2026-03-23 09:59:45 +0100

    test final

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 console.log("Projet système de surveillance");
 
 const CameraA = require("./src/sensors/CameraA");
+const TemperatureSensorA = require("./src/sensors/TemperatureSensorA");
+const ThermalSensorBAdapter = require("./src/adapters/ThermalSensorBAdapter");
 
-const cam = new CameraA("Salon");
+const EmailNotifier = require("./src/notifiers/EmailNotifier");
+const LogNotifier = require("./src/notifiers/LogNotifier");
+const DiscordNotifier = require("./src/notifiers/DiscordNotifier");
 
-cam.onDetect((msg) => {
-  console.log(msg);
-});
+const Room = require("./src/core/Room");
+
+// Création de la pièce
+const livingRoom = new Room("Salon");
+
+// Création des capteurs
+const camera = new CameraA("Salon");
+const tempSensor = new TemperatureSensorA("Salon", 30);
+const thermalSensor = new ThermalSensorBAdapter("B-01");
+
+// Ajout des capteurs à la pièce
+livingRoom.addSensor(camera);
+livingRoom.addSensor(tempSensor);
+livingRoom.addSensor(thermalSensor);
+
+// Création des notificateurs
+const emailNotifier = new EmailNotifier("securite@batiment.com");
+const logNotifier = new LogNotifier("alerts.log");
+const discordNotifier = new DiscordNotifier("Serveur-Surveillance");
+
+// Ajout des notificateurs à la pièce
+livingRoom.addNotifier(emailNotifier);
+livingRoom.addNotifier(logNotifier);
+livingRoom.addNotifier(discordNotifier);
+
+// Activation du système
+livingRoom.armSensors();
